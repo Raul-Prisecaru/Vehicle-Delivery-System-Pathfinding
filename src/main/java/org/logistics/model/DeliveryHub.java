3ba60a1580f1f4c20a9232ff56d9014cdb12d3ff
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-28 21:26:34 +0000

    Implemented: DeliveryHub Extends Node class

--- src/main/java/org/logistics/model/DeliveryHub.java
+++ src/main/java/org/logistics/model/DeliveryHub.java
@@ -1,4 +1,15 @@
 package org.logistics.model;
 
-public class DeliveryHub {
+public class DeliveryHub extends Node {
+    public DeliveryHub(int value) {
+        super(value);
+    }
+
+    public int get_node_value() {
+        return super.get_node_value();
+    }
+
+    public void set_node_value(int new_value) {
+        super.set_node_value(new_value);
+    }
 }
