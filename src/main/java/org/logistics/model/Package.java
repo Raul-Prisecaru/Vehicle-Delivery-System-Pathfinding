7da7b8f0c53ca7a33f998b46b1fb9bb86371f2a2
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-29 23:15:16 +0000

    Added: Added JavaDoc to Methods

--- src/main/java/org/logistics/model/Package.java
+++ src/main/java/org/logistics/model/Package.java
@@ -12,26 +12,53 @@ public class Package {
         this.priority = priority;
     }
 
+    /**
+     * Method to retrieve the Name of the Package
+     * @return Package name
+     */
     public String getItem_Name() {
         return this.item_Name;
     }
 
+    /**
+     * Method to configure the name of the Package
+     * @param newItem_Name String | New Name for the Package
+     * @return None
+     */
     public void setItem_Name(String newItem_Name) {
         this.item_Name = newItem_Name;
     }
 
+    /**
+     * Method to retrieve the destination of the Package
+     * @return CustomerLocation | Destination
+     */
     public CustomerLocation getDestination() {
         return this.destination;
     }
 
+    /**
+     * Method to configure the destination of the package
+     * @param new_Destionation CustomerLocation | Destination to delivery the package
+     * @return None
+     */
     public void setDestination(CustomerLocation new_Destionation) {
         this.destination = new_Destionation;
     }
 
+    /**
+     * Method to retrieve the priority of the Package
+     * @return int | Priority
+     */
     public int getPriority() {
         return this.priority;
     }
 
+    /**
+     * Method to configure the priority of the package
+     * @param priority int | Destination to delivery the package
+     * @return None
+     */
     public void setPriority(int priority) {
         this.priority = priority;
     }
