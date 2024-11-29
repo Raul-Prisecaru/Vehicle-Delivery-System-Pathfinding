8ac3443956893d339170e90421428ed9cc1a9bfa
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-29 21:41:58 +0000

    Added: Added priority option to the constructors and implemented setters

--- src/main/java/org/logistics/model/Vehicle.java
+++ src/main/java/org/logistics/model/Vehicle.java
@@ -1,4 +1,15 @@
 package org.logistics.model;
 
+
 public class Vehicle {
+    // TODO: Convert Queue to a Priority Queue Later On
+    private Package delivery_Packages;
+    private
+
+
+    public Vehicle(Package package_delivery) {
+        delivery_Packages = new Queue<Package>();
+
+        delivery_Packages.add(package_delivery);
+    }
 }
