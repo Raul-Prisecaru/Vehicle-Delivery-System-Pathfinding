aed004c39de19839ab3b84cb7e4d50a9175847b3
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-29 08:22:06 +0000

    Implemented: Basic Constructor for Package Class

--- src/main/java/org/logistics/model/Package.java
+++ src/main/java/org/logistics/model/Package.java
@@ -1,4 +1,12 @@
 package org.logistics.model;
 
 public class Package {
+    private String item_Name;
+    private CustomerLocation destination;
+
+    public Package(String item_Name, CustomerLocation destination) {
+        this.item_Name = item_Name;
+        this.destination = destination;
+    }
+
 }
