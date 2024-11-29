092893ef61e0d504a65af40e18cbeedd513fae23
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-29 23:37:04 +0000

    Implemented: Getters and Setters empty body methods with JavaDoc

--- src/main/java/org/logistics/model/Vehicle.java
+++ src/main/java/org/logistics/model/Vehicle.java
@@ -10,5 +10,22 @@ public class Vehicle {
         deliveryPackages = new PriorityQueue<>();
     }
 
+    /**
+     * Method to retrieve Packages from the Vehicle
+     * @return PriorityQueue | Priority Queue with Packages
+     */
+    public PriorityQueue<Package> get_deliveryPackages() {
+
+    }
+
+    /**
+     * Method to Add Packages to the Vehicle delivery job
+     * @param package_Delivery Package | Package to add to the Deliver Job
+     */
+    public void add_deliveryPackage(Package package_Delivery){
+
+    }
+
+
 
 }
