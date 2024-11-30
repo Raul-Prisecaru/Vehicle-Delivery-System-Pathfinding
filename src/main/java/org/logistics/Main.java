cd793c5a79b5e194d309fa3a795f6625205dbac4
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-30 00:40:13 +0000

    Moved: Moved Basic Simulation code from Graph to Main file

--- src/main/java/org/logistics/Main.java
+++ src/main/java/org/logistics/Main.java
@@ -1,7 +1,44 @@
 package org.logistics;
 
+import org.logistics.model.CustomerLocation;
+import org.logistics.model.DeliveryHub;
+import org.logistics.model.Edge;
+import org.logistics.model.Graph;
+
 public class Main {
     public static void main(String[] args) {
+        // Creating Graph Environment
+        Graph graph = new Graph();
 
+        // Creating DeliveryHub Nodes
+        DeliveryHub deliveryHub1 = new DeliveryHub(1);
+        DeliveryHub deliveryHub2 = new DeliveryHub(2);
+
+        // Creating CustomerLocation Nodes
+        CustomerLocation customerLocation1 = new CustomerLocation(1);
+        CustomerLocation customerLocation2 = new CustomerLocation(2);
+
+        // Creating Edges
+        Edge edge1 = new Edge(deliveryHub1, customerLocation1, 4, 2, 5);
+        Edge edge2 = new Edge(deliveryHub1, deliveryHub2, 5, 3, 9);
+
+        Edge edge3 = new Edge(deliveryHub2, customerLocation2, 5, 2, 5);
+        Edge edge4 = new Edge(deliveryHub2, deliveryHub1, 2, 6, 1);
+
+        // Adding Vertexs to the Hashmap
+        graph.add_Vertex(deliveryHub1);
+        graph.add_Vertex(deliveryHub2);
+
+        graph.add_Vertex(customerLocation1);
+        graph.add_Vertex(customerLocation2);
+
+        // Adding Edges to the hashmap
+        graph.add_Edge(edge1);
+        graph.add_Edge(edge2);
+        graph.add_Edge(edge3);
+        graph.add_Edge(edge4);
+
+        graph.print_List();
     }
+
 }
