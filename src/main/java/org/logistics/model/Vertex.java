cd9fd56c344ff357393d074172ff572c20c40ccc
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-06 08:49:29 +0000

    Implemented: added distance variable containing max value with getters and setters for it

--- src/main/java/org/logistics/model/Vertex.java
+++ src/main/java/org/logistics/model/Vertex.java
@@ -3,6 +3,7 @@ package org.logistics.model;
 
 public class Vertex {
     private int value;
+    private int distance = Integer.MAX_VALUE;
 
     public Vertex(int value){
         this.value = value;
@@ -14,6 +15,14 @@ public class Vertex {
 
     public void set_node_value(int new_value) {
         this.value = new_value;
+    }
+
+    public int get_distance() {
+        return distance;
+    }
+
+    public void set_distance(int new_distance) {
+        this.distance = new_distance;
     }
 
     
