3ffe40f657c889277e41630d83a5ca28d482a297
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-06 22:57:38 +0000

    BugFix: PathFinding algorithm wouldn't run again because I forgot to add to the unvisited priorityQueue and did a .poll() method before the loop which would break it

--- src/main/java/org/logistics/model/Graph.java
+++ src/main/java/org/logistics/model/Graph.java
@@ -73,11 +73,10 @@ public class Graph {
         PriorityQueue<Vertex> unvisited = new PriorityQueue<>(Comparator.comparingInt(Vertex::get_distance));
         Queue<Vertex> visited = new LinkedList<>();
 
-        Vertex current = unvisited.poll();
+        unvisited.add(start_vertex);
 
         while (!unvisited.isEmpty()) {
-
-
+            Vertex current = unvisited.poll();
             if (!visited.contains(current)) {
                 for (Edge edge : adjacencyList.get(current)) {
                     int totalDistance = current.get_distance() + edge.getDistance_weight();
