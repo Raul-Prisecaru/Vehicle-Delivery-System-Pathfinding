2c01472d21a2817112e5a58f0e5a68a5fad0cb8b
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-06 19:47:23 +0000

    Changed: Changed end_node -> connecting_node and commented out other edge weights instead of distance

--- src/main/java/org/logistics/model/Edge.java
+++ src/main/java/org/logistics/model/Edge.java
@@ -3,45 +3,45 @@ package org.logistics.model;
 
 public class Edge {
     // Nodes
-    private Node start_node;
-    private Node end_node;
+    private Vertex start_vertex;
+    private Vertex connecting_vertex;
 
     // Weights
     private int distance_weight;
-    private int traffic_weight;
-    private int time_weight;
+//    private int traffic_weight;
+//    private int time_weight;
 
-    public Edge(Node start_node, Node end_node, int distance_weight, int traffic_weight, int time_weight) {
-        this.start_node = start_node;
-        this.end_node = end_node;
+    public Edge(Vertex start_vertex, Vertex connecting_vertex, int distance_weight) {
+        this.start_vertex = start_vertex;
+        this.connecting_vertex = connecting_vertex;
         this.distance_weight = distance_weight;
-        this.traffic_weight = traffic_weight;
-        this.time_weight = time_weight;
+//        this.traffic_weight = traffic_weight;
+//        this.time_weight = time_weight;
     }
 
-    public Node getStart_node() {
-        return start_node;
+    public Vertex getStart_node() {
+        return start_vertex;
     }
 
-    public Node getEnd_node() {
-        return end_node;
+    public Vertex getConnecting_node() {
+        return connecting_vertex;
     }
 
-    public int getTime_weight() {
-        return time_weight;
-    }
-
-    public void setTime_weight(int time_weight) {
-        this.time_weight = time_weight;
-    }
-
-    public int getTraffic_weight() {
-        return traffic_weight;
-    }
-
-    public void setTraffic_weight(int traffic_weight) {
-        this.traffic_weight = traffic_weight;
-    }
+//    public int getTime_weight() {
+//        return time_weight;
+//    }
+//
+//    public void setTime_weight(int time_weight) {
+//        this.time_weight = time_weight;
+//    }
+//
+//    public int getTraffic_weight() {
+//        return traffic_weight;
+//    }
+//
+//    public void setTraffic_weight(int traffic_weight) {
+//        this.traffic_weight = traffic_weight;
+//    }
 
     public int getDistance_weight() {
         return distance_weight;
@@ -51,8 +51,8 @@ public class Edge {
         this.distance_weight = distance_weight;
     }
 
-    @Override
-    public String toString() {
-        return "{" + "Start Node: " +  this.start_node + ", " + "End Node: " + this.end_node + ", " + "distance: " + this.distance_weight + ", " + "traffic: " + this.traffic_weight + ", " + "time: " + this.time_weight + "}";
-    }
+//    @Override
+//    public String toString() {
+//        return "{" + "Start Node: " +  this.start_vertex + ", " + "End Node: " + this.end_vertex + ", " + "distance: " + this.distance_weight + ", " + "traffic: " + this.traffic_weight + ", " + "time: " + this.time_weight + "}";
+//    }
 }
