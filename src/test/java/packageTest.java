53bf5d552532bd13ee985aecd8750a40aa3132e5
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-11 16:57:36 +0000

    Changed: forgot to give the methods it's @Test annotation

--- src/test/java/packageTest.java
+++ src/test/java/packageTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.logistics.model.CustomerLocation;
 import org.logistics.model.Package;
 
@@ -8,15 +9,17 @@ public class packageTest {
     CustomerLocation customerLocationTest = new CustomerLocation("B");
     Package testPackage = new Package("TestItem", customerLocationTest, 1);
 
-
+    @Test
     void getItemName() {
         assertEquals("TestItem", testPackage.getItem_Name());
     }
 
+    @Test
     void getDestination() {
         assertEquals(customerLocationTest, testPackage.getDestination());
     }
 
+    @Test
     void getPriority() {
         assertEquals(1, testPackage.getPriority());
     }
