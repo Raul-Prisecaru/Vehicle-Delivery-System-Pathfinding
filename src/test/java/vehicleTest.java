e8e3d0ce7151c16fb7b3b5cf1c75944a8b3fce3c
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-11 15:29:36 +0000

    Added: vehicle Class for Unit Testing with empty body methods for testing

---
+++ src/test/java/vehicleTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.logistics.model.Vehicle;
+import org.logistics.model.Vertex;
+
+
+
+public class vehicleTest {
+
+    Vehicle testVehicle = new Vehicle(new Vertex("A"));
+
+    @Test
+    void getCurrentLocation() {
+
+    }
+
+    @Test
+    void updateCurrentLocation() {
+
+    }
+
+    @Test
+    void getDeliveryPackages() {
+
+    }
+
+    @Test
+    void updateDeliveryPackages() {
+
+    }
+
+    @Test
+    void getTravelDestination() {
+
+    }
+
+    @Test
+    void updateTravelDestination() {
+
+    }
+
+    @Test
+    void travelDestination() {
+
+    }
+
+}
