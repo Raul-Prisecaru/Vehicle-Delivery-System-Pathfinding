a97b775d176255b8d87cff9bd00485fb3b00118c
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-11 16:53:10 +0000

    Implemented: Code for Unit Testing package's methods

--- src/test/java/untested/packageTest.java
+++ src/test/java/untested/packageTest.java
@@ -1,24 +1,26 @@
 package untested;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.logistics.model.CustomerLocation;
 import org.logistics.model.Package;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class packageTest {
-    Package testPackage = new Package("TestItem", new CustomerLocation("B"), 1);
+    CustomerLocation customerLocationTest = new CustomerLocation("B");
+    Package testPackage = new Package("TestItem", customerLocationTest, 1);
 
 
     void getItemName() {
-
+        assertEquals("TestItem", testPackage.getItem_Name());
     }
 
     void getDestination() {
-
+        assertEquals(customerLocationTest, testPackage.getDestination());
     }
 
     void getPriority() {
-
+        assertEquals(1, testPackage.getPriority());
     }
 
 
