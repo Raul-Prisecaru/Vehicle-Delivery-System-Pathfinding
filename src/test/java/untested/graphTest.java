f7d3925d3e7e45f2f3761abea69dbc97aebc3267
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-11 17:26:55 +0000

    Implemented: Unit Tests for Graph's methods

--- src/test/java/untested/graphTest.java
+++ src/test/java/untested/graphTest.java
@@ -1,25 +1,49 @@
 package untested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.logistics.model.Graph;
 
+import org.logistics.model.Edge;
+import org.logistics.model.Graph;
+import org.logistics.model.Vertex;
+
+import java.util.LinkedList;
 
 
 public class graphTest {
     Graph graphTest = new Graph();
 
     @Test
+    void getAdjacencyList() {
+
+    }
+
+    @Test
     void addVertex() {
+        Vertex testVertex = new Vertex("A");
+
+        graphTest.add_vertex(testVertex);
+
+        assertTrue(graphTest.getAdjacencyList().containsKey(testVertex));
 
     }
 
     @Test
     void addDirectedEdge() {
+        Vertex testStartVertex = new Vertex("A");
+        Vertex testEndVertex = new Vertex("B");
+        Edge testEdge = new Edge(testStartVertex, testEndVertex, 10);
 
+        graphTest.add_vertex(testStartVertex);
+        graphTest.add_vertex(testEndVertex);
+        graphTest.add_directed_edge(testStartVertex, testEndVertex, 10);
+
+        Edge adjacentEdge = graphTest.getAdjacencyList().get(testStartVertex).getFirst();
+
+        assertEquals(testStartVertex, adjacentEdge.getStart_node());
+        assertEquals(testEndVertex, adjacentEdge.getConnecting_node());
+        assertEquals(10, adjacentEdge.getDistance_weight());
     }
 
-    @Test
-    void getAdjacencyList() {
 
-    }
+
 }
