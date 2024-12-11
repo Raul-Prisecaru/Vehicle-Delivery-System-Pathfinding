5b84b5acecd3f94639a2ab773405933189d57ded
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-11 16:45:21 +0000

    Changed: Removed Setters (Unneeded)

--- src/main/java/org/logistics/model/Package.java
+++ src/main/java/org/logistics/model/Package.java
@@ -21,15 +21,6 @@ public class Package {
     }
 
     /**
-     * Method to configure the name of the Package
-     * @param newItem_Name String | New Name for the Package
-     * @return None
-     */
-    public void setItem_Name(String newItem_Name) {
-        this.item_Name = newItem_Name;
-    }
-
-    /**
      * Method to retrieve the destination of the Package
      * @return CustomerLocation | Destination
      */
@@ -38,29 +29,11 @@ public class Package {
     }
 
     /**
-     * Method to configure the destination of the package
-     * @param new_Destionation CustomerLocation | Destination to delivery the package
-     * @return None
-     */
-    public void setDestination(CustomerLocation new_Destionation) {
-        this.destination = new_Destionation;
-    }
-
-    /**
      * Method to retrieve the priority of the Package
      * @return int | Priority
      */
     public int getPriority() {
         return this.priority;
-    }
-
-    /**
-     * Method to configure the priority of the package
-     * @param priority int | Destination to delivery the package
-     * @return None
-     */
-    public void setPriority(int priority) {
-        this.priority = priority;
     }
 
 
