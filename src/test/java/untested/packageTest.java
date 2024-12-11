8ce9ed2d101365f91708726c501ad64b2a04a455
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-11 16:48:30 +0000

    Added: configured test class for unit testing

--- src/test/java/untested/packageTest.java
+++ src/test/java/untested/packageTest.java
@@ -1,4 +1,25 @@
 package untested;
+import org.junit.jupiter.api.Test;
+import org.logistics.model.CustomerLocation;
+import org.logistics.model.Package;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class packageTest {
+    Package testPackage = new Package("TestItem", new CustomerLocation("B"), 1);
+
+
+    void getItemName() {
+
+    }
+
+    void getDestination() {
+
+    }
+
+    void getPriority() {
+
+    }
+
+
 }
