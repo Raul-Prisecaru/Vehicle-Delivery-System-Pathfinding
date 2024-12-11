867d037b8c9ffa50e89c23dd4793a55bb4eeb3cf
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-11 17:03:10 +0000

    Added: Configured class for Unit Testing

--- src/test/java/untested/graphTest.java
+++ src/test/java/untested/graphTest.java
@@ -1,4 +1,25 @@
 package untested;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.logistics.model.Graph;
+
+
 
 public class graphTest {
+    Graph graphTest = new Graph();
+
+    @Test
+    void addVertex() {
+
+    }
+
+    @Test
+    void addDirectedEdge() {
+
+    }
+
+    @Test
+    void getAdjacencyList() {
+
+    }
 }
