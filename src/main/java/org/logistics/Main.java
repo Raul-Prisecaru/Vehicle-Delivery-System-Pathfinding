3f7a1c9c355a6bbf0f79c312770a16aac703fe4e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-17 19:31:43 +0000

    Changed: Expanded vertexes and Edges to be better traversalable

--- src/main/java/org/logistics/Main.java
+++ src/main/java/org/logistics/Main.java
@@ -20,6 +20,8 @@ public class Main {
         CustomerLocation customerLocationC = new CustomerLocation("C");
         CustomerLocation customerLocationD = new CustomerLocation("D");
         CustomerLocation customerLocationE = new CustomerLocation("E");
+        CustomerLocation customerLocationF = new CustomerLocation("F");
+        CustomerLocation customerLocationG = new CustomerLocation("G");
 
         // Creating Packages
         Vehicle vehicle1 = new Vehicle(deliveryHubA);
@@ -33,15 +35,27 @@ public class Main {
         graph.add_vertex(customerLocationC);
         graph.add_vertex(customerLocationD);
         graph.add_vertex(customerLocationE);
+        graph.add_vertex(customerLocationF); // New Vertex
+        graph.add_vertex(customerLocationG); // New Vertex
 
         // Creating Edges
-        graph.add_directed_edge(deliveryHubA, deliveryHubB, 4);
-        graph.add_directed_edge(deliveryHubA, customerLocationC, 2);
-        graph.add_directed_edge(deliveryHubB, customerLocationC, 1);
-        graph.add_directed_edge(deliveryHubB, customerLocationD, 2);
-        graph.add_directed_edge(deliveryHubB, customerLocationE, 3);
-        graph.add_directed_edge(customerLocationC, customerLocationE, 1);
-        graph.add_directed_edge(customerLocationE, customerLocationD, 5);
+        graph.add_directed_edge(deliveryHubA, deliveryHubB, 4); // Distance: 4
+        graph.add_directed_edge(deliveryHubA, customerLocationC, 2); // Distance: 2
+        graph.add_directed_edge(deliveryHubB, customerLocationC, 1); // Distance: 1
+        graph.add_directed_edge(deliveryHubB, customerLocationD, 2); // Distance: 2
+        graph.add_directed_edge(deliveryHubB, customerLocationE, 3); // Distance: 3
+        graph.add_directed_edge(customerLocationC, customerLocationE, 1); // Distance: 1
+        graph.add_directed_edge(customerLocationE, customerLocationD, 5); // Distance: 5
+
+        // New Edges to Connect New Vertexes
+        graph.add_directed_edge(customerLocationE, customerLocationF, 3); // Distance: 3
+        graph.add_directed_edge(customerLocationF, customerLocationG, 2); // Distance: 2
+        graph.add_directed_edge(customerLocationG, deliveryHubA, 4); // Distance: 4
+
+        // Additional Edges to Ensure Reachability
+        graph.add_directed_edge(customerLocationD, customerLocationF, 2); // Distance: 2
+        graph.add_directed_edge(customerLocationC, customerLocationG, 6); // Distance: 6
+
 
 //        graph.print_List();
 //        pathfinding.find_shortest_path(vehicle1);
