e13b398639000517dfb8f7d113cb0d317ec2d2aa
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-18 13:38:25 +0000

    Implemented: Basic method to findVertex

--- src/main/java/org/logistics/model/Graph.java
+++ src/main/java/org/logistics/model/Graph.java
@@ -35,14 +35,25 @@ public class Graph {
      * @return None if successful, Error message otherwise
      */
     public void add_directed_edge(Vertex start_vertex, Vertex connecting_node, int distance_weight) {
+        Vertex exist_start = this.findVertex(start_vertex);
+        Vertex exist_connecting = this.findVertex(connecting_node);
+
+        if (exist_start == null) {
+            System.out.println("Vertex: " + start_vertex.get_node_value() + " Does not exist");
+            return;
+        }
+
+        if (exist_connecting == null) {
+            System.out.println("Vertex: " + connecting_node.get_node_value() + " Does not exist");
+            return;
+
+        }
         Edge vertex_edge = new Edge(start_vertex, connecting_node, distance_weight);
 
-         if (adjacencyList.get(vertex_edge.getStart_node()) != null) {
-            adjacencyList.get(vertex_edge.getStart_node()).add(vertex_edge);
-        } else {
-             // TODO: Change this to Throw instead Later on
-             System.out.println("Node:" + start_vertex.get_node_value() + " Does Not Exist");
-         }
+
+        adjacencyList.get(exist_start).add(vertex_edge);
+
+
     }
 
     /**
@@ -66,6 +77,27 @@ public class Graph {
     public HashMap<Vertex, LinkedList<Edge>> getAdjacencyList() {
         return adjacencyList;
     }
+
+    public Vertex findVertex(Vertex vertex) {
+        // TODO: Improve this to search for both so we don't have to reloop each time
+        System.out.println(vertex.get_node_value());
+        for (Vertex v : adjacencyList.keySet()) {
+            if (Objects.equals(v.get_node_value(), vertex.get_node_value())) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+//    public Boolean findVertex(Vertex vertex1, Vertex vertex2) {
+//        // TODO: Improve this to search for both so we don't have to reloop each time
+//        for (Vertex v : adjacencyList.keySet()) {
+//            if (v.get_node_value() == vertex1.get_node_value()) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
 }
 
