6796e976f4d1dae06d917b84102f0f9664427a66
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-27 22:35:16 +0000

    Implemented: Method to add VehicleLog

--- src/main/java/org/logistics/model/vehicleLogging.java
+++ src/main/java/org/logistics/model/vehicleLogging.java
@@ -1,4 +1,24 @@
 package org.logistics.model;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class vehicleLogging {
+
+    HashMap<Vehicle, LinkedList<String>> vehicleLogs;
+
+    public vehicleLogging() {
+        vehicleLogs = new HashMap<>();
+    }
+
+    public void addVehicleLog(Vehicle vehicle, String statusLog)  {
+        LinkedList<String> linkedList = new LinkedList<String>();
+
+        linkedList.add("Current Location: " + vehicle.getCurrent_location().getNodeValue());
+        linkedList.add("Travel Destination: " + vehicle.getTravelDestinations());
+        linkedList.add("Package: " + vehicle.get_deliveryPackages());
+        linkedList.add("Status:" + statusLog);
+
+        vehicleLogs.put(vehicle, linkedList);
+    }
 }
