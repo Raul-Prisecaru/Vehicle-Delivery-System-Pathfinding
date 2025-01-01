46bb0665a59e09d42b845b82a5c01a74d47a326d
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-01 03:42:48 +0000

    Feat: Created Unit Test class for multi-Agent feature and added relevant methods to the class for testing

---
+++ src/test/java/MultiAgentFeatureTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class MultiAgentFeatureTest {
+
+
+    void findMultipleVehiclesInCustomerLocations() {
+
+    }
+
+
+    void vehicleCommunicate() {
+
+    }
+}
