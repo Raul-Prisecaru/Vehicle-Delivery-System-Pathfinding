d1418e79ce09f9b741947f7079248ddec0216aae
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-01 02:28:09 +0000

    Feat: Added empty method with JavaDoc to allow vehicle to communicate

--- src/main/java/org/logistics/model/MultiAgent.java
+++ src/main/java/org/logistics/model/MultiAgent.java
@@ -9,14 +9,26 @@ public class MultiAgent {
 
     // TODO: For time being this will do but shorten the method name
     public CustomerLocation<String> findMultipleVehiclesInCustomerLocations() {
+
         for (CustomerLocation<String> customerLocation : graph.getAllCustomerLocation()) {
+
             if (customerLocation.getStoredVehicles().size() >= 2) {
+
                 // TODO: Make this return multiple
                 return customerLocation;
             }
         }
 
         return null;
+    }
+
+    // TODO: Probably improve description
+    /**
+     * Method Responsible for allowing the Vehicle to communicate and figure out if
+     * one of the packages from the vehicle1 is closer to vehicle2 path and vice versa
+     */
+    public void vehicleCommunicate() {
+
     }
 
 }
