8d648869946957aa79076c51ef8c1f076cf82dd6
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-01 02:06:36 +0000

    Feat: Created MultiAgent class with constructor and empty method to find multiple vehicles in a customerLocation

---
+++ src/main/java/org/logistics/model/MultiAgent.java
@@ -0,0 +1,15 @@
+package org.logistics.model;
+
+public class MultiAgent {
+    private Graph graph;
+
+    public MultiAgent(Graph graph) {
+        this.graph = graph;
+    }
+
+    // TODO: For time being this will do but shorten the method name
+    public void findMultipleVehiclesInCustomerLocations() {
+
+    }
+
+}
