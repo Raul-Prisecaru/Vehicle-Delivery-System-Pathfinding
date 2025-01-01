9a2c42cebcacee6ea1232df765f21d7fc854e947
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-01 03:53:14 +0000

    Feat: Created specialized graph environment for findMultipleVehiclesInCustomerLocation method unit test

--- src/test/java/MultiAgentFeatureTest.java
+++ src/test/java/MultiAgentFeatureTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.logistics.model.*;
+import org.logistics.model.Package;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -6,8 +9,37 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MultiAgentFeatureTest {
 
 
-    void findMultipleVehiclesInCustomerLocations() {
+    void findMultipleVehiclesInCustomerLocations() throws Exception {
+        Graph graph = new Graph();
 
+
+        // Creating CustomerLocation Nodes
+        CustomerLocation<String> customerLocationA = new CustomerLocation<>("A");
+        CustomerLocation<String> customerLocationB = new CustomerLocation<>("B");
+        CustomerLocation<String> customerLocationC = new CustomerLocation<>("C");
+        CustomerLocation<String> customerLocationD = new CustomerLocation<>("D");
+        CustomerLocation<String> customerLocationE = new CustomerLocation<>("E");
+
+        // Creating Packages
+        Vehicle vehicle1 = new Vehicle(customerLocationC);
+        Vehicle vehicle2 = new Vehicle(customerLocationC);
+
+        graph.add_customerLocation(customerLocationA);
+        graph.add_customerLocation(customerLocationB);
+        graph.add_customerLocation(customerLocationC);
+        graph.add_customerLocation(customerLocationD);
+        graph.add_customerLocation(customerLocationE);
+
+        // Creating Edges
+        graph.add_directed_edge(customerLocationA, customerLocationB, 5);
+        graph.add_directed_edge(customerLocationB, customerLocationE, 5);
+        graph.add_directed_edge(customerLocationE, customerLocationD, 5);
+        graph.add_directed_edge(customerLocationD, customerLocationA, 5);
+
+        graph.add_directed_edge(customerLocationC, customerLocationA,5);
+        graph.add_directed_edge(customerLocationC, customerLocationB,5);
+        graph.add_directed_edge(customerLocationC, customerLocationD,5);
+        graph.add_directed_edge(customerLocationC, customerLocationE,5);
     }
 
 
