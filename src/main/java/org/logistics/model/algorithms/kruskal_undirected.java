4e63d9a10783d3f68262c3a0210bc2bc9d07130d
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-03 23:36:51 +0000

    Feat: Added getWeightsIncreasingOrder and generateStartVertexesForHashMap methods

--- src/main/java/org/logistics/model/algorithms/kruskal_undirected.java
+++ src/main/java/org/logistics/model/algorithms/kruskal_undirected.java
@@ -1,15 +1,12 @@
 package org.logistics.model.algorithms;
 
-import org.logistics.model.CustomerLocation;
-import org.logistics.model.DeliveryHub;
-import org.logistics.model.Graph;
-import org.logistics.model.Vertex;
+import org.logistics.model.*;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class kruskal_undirected {
     private HashMap<Vertex<String>, Vertex<String>> vertexSets = new HashMap<>();
+    private HashMap<Integer, Edge> weights = new HashMap<>();
 
     /**
      * Constructor
@@ -18,12 +15,28 @@ public class kruskal_undirected {
 
     }
 
+
+
     /**
      * Method Responsible for finding MST of the undirected Graph
      */
     public void find_MST(Graph undirected_graph) {
         vertexSets.clear();
 
+        // Initial Stuff
+        generateStartVertexesForHashMap(undirected_graph);
+
+    }
+
+    public Vertex<String> getPointerPair(Vertex<String> vertex) {
+        return vertexSets.get(vertex);
+    }
+
+    public void getWeightsIncreasingOrder() {
+
+    }
+
+    public void generateStartVertexesForHashMap(Graph undirected_graph) {
         for (DeliveryHub<String> deliveryHub : undirected_graph.getAllDeliveryHub()) {
             vertexSets.put(deliveryHub, deliveryHub);
         }
@@ -31,13 +44,6 @@ public class kruskal_undirected {
         for (CustomerLocation<String> customerLocation : undirected_graph.getAllCustomerLocation()) {
             vertexSets.put(customerLocation, customerLocation);
         }
-
-
-
-    }
-
-    public Vertex<String> getPointerPair(Vertex<String> vertex) {
-        return vertexSets.get(vertex);
     }
 
 
