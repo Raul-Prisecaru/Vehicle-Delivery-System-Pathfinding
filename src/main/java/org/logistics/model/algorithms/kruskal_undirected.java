f1fc8322b30b893de33be98beb4a202ebe581f85
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-03 23:01:08 +0000

    Feat: Created kruskal_undirected class with constructor and method with simple JavaDoc

---
+++ src/main/java/org/logistics/model/algorithms/kruskal_undirected.java
@@ -0,0 +1,19 @@
+package org.logistics.model.algorithms;
+
+public class kruskal_undirected {
+
+    /**
+     * Constructor
+     */
+    public kruskal_undirected() {
+
+    }
+
+    /**
+     * Method Responsible for finding MST of the undirected Graph
+     */
+    public void find_MST() {
+
+    }
+
+}
