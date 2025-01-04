44da971ec029a1cd28b0404dde4b62ae164f493c
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-04 23:27:34 +0000

    Feat: Created Main File and added print statements

---
+++ src/main/java/org/logistics/main.java
@@ -0,0 +1,15 @@
+package org.logistics;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("-- Currently there are two simulation available --");
+        System.out.println("- Simulation 1: Directed, Dijkstra Algorithm, Bellman-ford");
+        System.out.println("- Simulation 2: Undirected, Kruskal Algorithm");
+        System.out.print("Which Simulation would you like to run: ");
+
+
+    }
+}
