c76c4d8e3704d53efd68ece8e8aabf1742aa3889
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-04 23:36:15 +0000

    Feat: Added scanner to collect user input and applied switch case to run the relevant simulation

--- src/main/java/org/logistics/main.java
+++ src/main/java/org/logistics/main.java
@@ -1,15 +1,28 @@
 package org.logistics;
 
+import org.logistics.controller.directedGraphSimulation;
+import org.logistics.controller.undirectedGraphSimulation;
+
 import java.util.Scanner;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("-- Currently there are two simulation available --");
         System.out.println("- Simulation 1: Directed, Dijkstra Algorithm, Bellman-ford");
         System.out.println("- Simulation 2: Undirected, Kruskal Algorithm");
         System.out.print("Which Simulation would you like to run: ");
 
+        int simulation_option = scanner.nextInt();
 
+        switch (simulation_option) {
+            case 1:
+                org.logistics.controller.directedGraphSimulation.runSimulation();
+                break;
+
+            case 2:
+                org.logistics.controller.undirectedGraphSimulation.runSimulation();
+        }
     }
 }
