d0d81b1309f970d400dc19a2012b63ffdc58c47d
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-05 07:47:40 +0000

    Feat: Added constructor and method for calculating congestion with javaDoc

--- src/main/java/org/logistics/model/CongestionPrediction.java
+++ src/main/java/org/logistics/model/CongestionPrediction.java
@@ -4,4 +4,20 @@ package org.logistics.model;
  * Class Responsible for predicting Congestions on Edges
  */
 public class CongestionPrediction {
+    private Graph graph;
+
+    /**
+     * Constructor
+     * @param graph
+     */
+    public CongestionPrediction(Graph graph) {
+        this.graph = graph;
+    }
+
+    /**
+     * Method Responsible for calculating congestion prediction of provided edge
+     */
+    public void calculateCongestion() {
+
+    }
 }
