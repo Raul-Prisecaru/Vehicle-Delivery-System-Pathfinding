e85344052387b39a18e5565b6426ed337a4d4857
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-05 00:32:12 +0000

    Feat: Removed Static from method

--- src/main/java/org/logistics/controller/undirectedGraphSimulation.java
+++ src/main/java/org/logistics/controller/undirectedGraphSimulation.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 public class undirectedGraphSimulation {
 
-    public static void runSimulation() throws Exception {
+    public void runSimulation() throws Exception {
 
         // Initializing Graph
         Graph graph = new Graph();
