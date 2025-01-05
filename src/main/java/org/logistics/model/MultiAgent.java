0a010f034970a3b0b41dd00168e540ff084adb57
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-05 06:54:11 +0000

    Feat: Little code clean-up for optimisePackages method

--- src/main/java/org/logistics/model/MultiAgent.java
+++ src/main/java/org/logistics/model/MultiAgent.java
@@ -46,44 +46,43 @@ public class MultiAgent {
 
                 for (int j = i + 1; j < vehicleList.size(); j++) {
                     Vehicle vehicle2 = vehicleList.get(j);
+
+                    optimisePackages(vehicle1, vehicle2);
+
                 }
             }
         }
     }
 
     private void optimisePackages(Vehicle vehicle1, Vehicle vehicle2) throws Exception {
-            // TODO: Optimise this so it can handle more than 3 vehicles
-            Vehicle vehicle1 = vehicleList.getFirst();
-            Vehicle vehicle2 = vehicleList.getLast();
 
-            if (vehicle1.get_deliveryPackages().size() < 2) {
-                for (Package package_package : vehicle2.get_deliveryPackages()) {
-                    int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
-                    int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
+        if (vehicle1.get_deliveryPackages().size() < 2) {
+            for (Package package_package : vehicle2.get_deliveryPackages()) {
+                int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
+                int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
 
-                    if (vehicle1Steps != -1 && vehicle2Steps != -1) {
+                if (vehicle1Steps != -1 && vehicle2Steps != -1) {
                         if (vehicle1Steps < vehicle2Steps) {
                             vehicle2.remove_deliveryPackage(package_package);
                             vehicle1.add_deliveryPackage(package_package);
                         }
                     }
-                }
             }
+        }
 
-            if (vehicle2.get_deliveryPackages().size() < 2) {
-                for (Package package_package : vehicle1.get_deliveryPackages()) {
-                    int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
-                    int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
 
-                    if (vehicle1Steps != -1 && vehicle2Steps != -1) {
-                        if (vehicle2Steps < vehicle1Steps) {
-                            vehicle1.remove_deliveryPackage(package_package);
-                            vehicle2.add_deliveryPackage(package_package);
-                        }
+        if (vehicle2.get_deliveryPackages().size() < 2) {
+            for (Package package_package : vehicle1.get_deliveryPackages()) {
+                int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
+                int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
+
+                if (vehicle1Steps != -1 && vehicle2Steps != -1) {
+                    if (vehicle2Steps < vehicle1Steps) {
+                        vehicle1.remove_deliveryPackage(package_package);
+                        vehicle2.add_deliveryPackage(package_package);
                     }
                 }
             }
         }
     }
-
 }
