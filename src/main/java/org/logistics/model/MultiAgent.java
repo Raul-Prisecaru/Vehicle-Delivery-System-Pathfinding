4f95539b6e218f16bdbb44ff785b62d93208b60f
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-05 06:46:31 +0000

    Feat: Broke down vehicleCommunicate method into two methods. VehicleCommunicate now takes HashSet and loops through each customerLocation and loops through vehicleList to get the first and second vehicles

--- src/main/java/org/logistics/model/MultiAgent.java
+++ src/main/java/org/logistics/model/MultiAgent.java
@@ -23,48 +23,63 @@ public class MultiAgent {
         return multiCustomerLocations;
     }
 
-    // TODO: Probably improve description
     /**
      * Method Responsible for allowing the Vehicle to communicate and figure out if
      * one of the packages from the vehicle1 is closer to vehicle2 path and vice versa
      */
-    public void vehicleCommunicate(CustomerLocation<String> customerLocation) throws Exception {
+    public void vehicleCommunicate(HashSet<CustomerLocation<String>> customerLocationHashSet) throws Exception {
         ArrayList<Vehicle> vehicleList = new ArrayList<>();
 
-        if (customerLocation.getStoredVehicles().size() < 2) {
-            return;
-        }
-        for (Vehicle vehicle : customerLocation.getStoredVehicles()) {
-            vehicleList.add(vehicle);
-        }
+        for (CustomerLocation<String> customerLocation : customerLocationHashSet) {
+            vehicleList.clear();
 
-        // TODO: Optimise this so it can handle more than 3 vehicles
-        Vehicle vehicle1 = vehicleList.getFirst();
-        Vehicle vehicle2 = vehicleList.getLast();
+            if (customerLocation.getStoredVehicles().size() < 2) {
+                return;
+            }
 
-        if (vehicle1.get_deliveryPackages().size() < 2) {
-            for (Package package_package : vehicle2.get_deliveryPackages()) {
-                int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
-                int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
+            for (Vehicle vehicle : customerLocation.getStoredVehicles()) {
+                vehicleList.add(vehicle);
+            }
 
-                if (vehicle1Steps != -1 && vehicle2Steps != -1) {
-                    if (vehicle1Steps < vehicle2Steps) {
-                        vehicle2.remove_deliveryPackage(package_package);
-                        vehicle1.add_deliveryPackage(package_package);
-                    }
+            for (int i = 0; i < vehicleList.size(); i++) {
+                Vehicle vehicle1 = vehicleList.get(i);
+
+                for (int j = i + 1; j < vehicleList.size(); j++) {
+                    Vehicle vehicle2 = vehicleList.get(j);
                 }
             }
         }
+    }
 
-        if (vehicle2.get_deliveryPackages().size() < 2) {
-            for (Package package_package : vehicle1.get_deliveryPackages()) {
-                int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
-                int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
+    private void optimisePackages(Vehicle vehicle1, Vehicle vehicle2) throws Exception {
+            // TODO: Optimise this so it can handle more than 3 vehicles
+            Vehicle vehicle1 = vehicleList.getFirst();
+            Vehicle vehicle2 = vehicleList.getLast();
 
-                if (vehicle1Steps != -1 && vehicle2Steps != -1) {
-                    if (vehicle2Steps < vehicle1Steps) {
-                        vehicle1.remove_deliveryPackage(package_package);
-                        vehicle2.add_deliveryPackage(package_package);
+            if (vehicle1.get_deliveryPackages().size() < 2) {
+                for (Package package_package : vehicle2.get_deliveryPackages()) {
+                    int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
+                    int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
+
+                    if (vehicle1Steps != -1 && vehicle2Steps != -1) {
+                        if (vehicle1Steps < vehicle2Steps) {
+                            vehicle2.remove_deliveryPackage(package_package);
+                            vehicle1.add_deliveryPackage(package_package);
+                        }
+                    }
+                }
+            }
+
+            if (vehicle2.get_deliveryPackages().size() < 2) {
+                for (Package package_package : vehicle1.get_deliveryPackages()) {
+                    int vehicle1Steps = vehicle1.findTravelSteps(package_package.getDestination());
+                    int vehicle2Steps = vehicle2.findTravelSteps(package_package.getDestination());
+
+                    if (vehicle1Steps != -1 && vehicle2Steps != -1) {
+                        if (vehicle2Steps < vehicle1Steps) {
+                            vehicle1.remove_deliveryPackage(package_package);
+                            vehicle2.add_deliveryPackage(package_package);
+                        }
                     }
                 }
             }
