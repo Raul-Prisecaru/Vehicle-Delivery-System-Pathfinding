7f79a41e7fb46747633695da02e9fd52e1e7b80a
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-02-01 22:42:41 +0000

    Feat: Added Logs

--- src/test/java/MultiAgentFeatureTest.java
+++ src/test/java/MultiAgentFeatureTest.java
@@ -95,14 +95,39 @@ public class MultiAgentFeatureTest {
         graph.add_directed_edge(customerLocationC, customerLocationB,5);
         graph.add_directed_edge(customerLocationC, customerLocationD,5);
         graph.add_directed_edge(customerLocationC, customerLocationE,5);
-
         customerLocationC.addStoredVehicles(vehicle1);
         customerLocationC.addStoredVehicles(vehicle2);
+
+        System.out.println("-- Before --");
+        System.out.println("Current Packages of Vehicles:");
+        System.out.println("vehicle" + vehicle1);
+        for (Package package_package : vehicle1.get_deliveryPackages()) {
+            System.out.println("Item Name: " + package_package.getItem_Name());
+            System.out.println("Destination: " + package_package.getDestination());
+        }
+
+        System.out.println("vehicle" + vehicle2);
+        for (Package package_package : vehicle2.get_deliveryPackages()) {
+            System.out.println("Item Name: " + package_package.getItem_Name());
+            System.out.println("Destination: " + package_package.getDestination());
+        }
 
         MultiAgent multiAgent = new MultiAgent(graph);
         HashSet<CustomerLocation<String>> foundCustomerLocation = multiAgent.findMultipleVehiclesInCustomerLocations();
         multiAgent.vehicleCommunicate(foundCustomerLocation);
 
+        System.out.println("-- after --");
+        System.out.println(vehicle1);
+        for (Package package_package : vehicle1.get_deliveryPackages()) {
+            System.out.println("Item Name: " + package_package.getItem_Name());
+            System.out.println("Destination: " + package_package.getDestination());
+        }
+
+        System.out.println(vehicle2);
+        for (Package package_package : vehicle2.get_deliveryPackages()) {
+            System.out.println("Item Name: " + package_package.getItem_Name());
+            System.out.println("Destination: " + package_package.getDestination());
+        }
 
         for (Package package_package : vehicle1.get_deliveryPackages()) {
                 assertEquals(package_package, vehicle2Package);
